542f3e20eef82d898b41a97c88a0aa33be2ab9d7
Noah <noahmwangi@gmail.com>
2023-03-25 08:53:44 +0300

    Added a delete button to the tasks

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ let form = document.getElementById('create-task-form');
 let tasks = document.getElementById("tasks");
 form.addEventListener("submit", function(event) {
   event.preventDefault();
-  var taskInput = document.getElementById('new-task-description');
+  let taskInput = document.getElementById('new-task-description');
   
-var addTask = function () {
-  var text = taskInput.value;
-  var li = document.createElement('li');
+let addTask = function () {
+  let text = taskInput.value;
+  let li = document.createElement('li');
+  let deleteButton = document.createElement('button');
   li.innerHTML = text;
+  deleteButton.innerHTML = "DELETE";
+  deleteButton.onclick = function() {
+    var parent = this.parentNode.parentNode; /*this will set the display div as the parent with respect to the button.*/
+    var child = this.parentNode;/*the li element will now beassigned to the variable child*/
+    parent.removeChild(child); /*rWe use this method to delete the selected li element from the DOM*/
+  }  
+  li.appendChild(deleteButton);
   tasks.appendChild(li);
 }
 addTask();
