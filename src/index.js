1835c55b2792c04bc4e99134a402c626b8a6563c
Noah <noahmwangi@gmail.com>
2023-03-25 08:34:07 +0300

    completed first part

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+let form = document.getElementById('create-task-form');
+let tasks = document.getElementById("tasks");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var taskInput = document.getElementById('new-task-description');
+  
+var addTask = function () {
+  var text = taskInput.value;
+  var li = document.createElement('li');
+  li.innerHTML = text;
+  tasks.appendChild(li);
+}
+addTask();
+taskInput.value = ' ';
+})
+})
